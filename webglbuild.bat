a620a27916464969d3e520cf8bc7cdb138c8c2f4
nd6k <robbie.monty@hotmail.com>
2024-02-05 14:24:57 +0000

    first commit

---
+++ webglbuild.bat
@@ -0,0 +1,6 @@
+@REM @ECHO OFF
+cargo build --target wasm32-unknown-unknown --release
+
+wasm-bindgen .\target\wasm32-unknown-unknown\release\brackets-web-test.wasm --out-dir .\wasm_help\staging --no-modules --no-typescript
+
+serve wasm_help
