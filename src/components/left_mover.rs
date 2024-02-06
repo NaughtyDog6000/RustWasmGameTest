2768d5e043ce1b44932f47fca4a6d404f0dbf705
nd6k <robbie.monty@hotmail.com>
2024-02-06 18:28:39 +0000

    ecs

---
+++ src/components/left_mover.rs
@@ -0,0 +1,5 @@
+use specs::{prelude::*, Component};
+
+#[derive(Component)]
+pub struct LeftMover {}
+
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,72 @@
-use web_time::{Instant, SystemTime};
 use bracket_terminal::prelude::*;
+use components::left_mover::LeftMover;
+use specs::{prelude::*, Component};
+use systems::left_walker::LeftWalker;
+use web_time::Instant;
 
-struct Birb {
+mod entities;
+mod components;
+mod systems;
+
+#[derive(Component)]
+pub struct Position {
     x: i32,
     y: i32,
-    // rotation: i32,
 }
 
-struct Logic {
-    // the time that the last logic tick happened
-    next_logic_tick: u128,
-    start_instant: Instant,
-    // the time between logic ticks in ms.
-    logic_tick_time: u128,
+#[derive(Component)]
+pub struct Renderable {
+    glyph: bracket_terminal::FontCharType,
+    fg: RGB,
+    bg: RGB,
 }
 struct State {
-    player: Birb,
-    game_logic: Logic, 
+    ecs: World,
+    next_logic_tick: u128,
+    start_instant: Instant,
+    logic_tick_interval: u128,
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+
+    fn new() -> State {
+        State {
+            ecs: World::new(),
+            start_instant: web_time::Instant::now(),
+            logic_tick_interval: 500,
+            next_logic_tick: 0
+        }
+    }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        let now = self.start_instant.elapsed().as_millis();
+
         // clear the screen and set background
         ctx.cls_bg(RGB::from_f32(0.5, 0.5, 1.0));
         // display the framtime in the top left
         ctx.print(0, 0, ctx.frame_time_ms);
 
-        let now = self.game_logic.start_instant.elapsed().as_millis();
-        // if enough time has passed since the last logic tick then run the logic, otherwise re-render the current frame.
-        if now > self.game_logic.next_logic_tick {
-            self.game_logic.next_logic_tick = now + self.game_logic.logic_tick_time;
-            self.player.x += 1;
-
+        // if it is a logic tick then run the logic systems
+        if now > self.next_logic_tick {
+            self.next_logic_tick = now + self.logic_tick_interval;
+            self.run_systems();
         }
 
         ctx.print(1, 1, "Hello Bracket World");
-        ctx.print_color(self.player.x, self.player.y, RGB::named(WHITE), RGB::named(RED), "P");
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
@@ -46,6 +77,34 @@ fn main() -> BError {
         .with_fullscreen(false)
         .build()?;
 
-    let gs: State = State { player: Birb { x: 0, y: 10 }, game_logic: Logic {next_logic_tick: 0, logic_tick_time: 500, start_instant: web_time::Instant::now() } };
+    let mut gs = State::new();
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+
+    // Create Player
+    gs.ecs.create_entity()
+    .with(Position {x: 40, y: 25})
+    .with(Renderable {
+        glyph: to_cp437('P'),
+        bg: RGB::from_u8(0,0,0),
+        fg: RGB::from_u8(255, 255, 0),
+    })
+    .build();
+
+    // create moving tubes
+    for i in 0..10 {
+        gs.ecs.create_entity()
+        .with(Position { x: i * 6 + 20, y: 20 })
+        .with(Renderable {
+            glyph: to_cp437('@'),
+            fg: RGB::from_u8(0, 255, 0),
+            bg: RGB::new(),
+        })
+        .with(LeftMover{})
+        .build();
+    }
+
+
     main_loop(context, gs)
 }
